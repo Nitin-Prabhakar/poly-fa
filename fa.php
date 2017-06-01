5fd3030ff881fb200ddc283ac734b53e9ab3999f
Nitin <omitwithnitin@gmail.com>
2017-06-01 11:53:25 +0530

    1.Add fetch events 2. Make CURL GET FA 3. Cache on install of service worker

--- fa.php
+++ fa.php
@@ -1,3 +1,23 @@
 <?php
+if(isset($_POST['getFA']) && $_POST['getFA']==1){
+  // init the resource
+$ch = curl_init();
 
+// set options...
+curl_setopt($ch, CURLOPT_URL, "https://use.fontawesome.com/4ed3881c04.css");
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+
+// execute
+$output = curl_exec($ch);
+$fa = DIRECTORY_SEPARATOR."fa.css";
+if(file_exists($fa))
+  unlink( $fa );
+$poly_fa = fopen($fa, "w+");
+fwrite($poly_fa, $output);
+fclose($poly_fa);
+// free
+curl_close($ch);
+echo "fetched";
+}
+exit;
  ?>
--- fa_sw.js
+++ fa_sw.js
@@ -1,11 +1,18 @@
 var FA = 's-0-q-cache-FA';
 self.addEventListener( 'install', function ( event ) {
     self.skipWaiting();
-    fetch( 'https://use.fontawesome.com/4ed3881c04.css', {
-            mode: 'no-cors'
+    fetch( '/fa.php', {
+            method: 'post',
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            body: 'getFA=1'
         } )
-        .then( function ( stylesheet ) {
-            console.log( stylesheet, stylesheet.text() );
+        .then( function ( response ) {
+            caches.open( FA )
+                .then( function ( cache ) {
+                    return cache.addAll( [ '/fa.css' ] );
+                } )
         } );
 } );
 self.addEventListener( 'activate', function ( event ) {} );
@@ -13,4 +20,38 @@ self.addEventListener( 'message', function ( event ) {
     var interrupt = JSON.parse( event.data );
     if ( interrupt[ 'yes' ] == 1 )
         importScripts( interrupt[ 'scriptURL' ] );
+} );
+importScripts( interrupt[ 'scriptURL' ] );
+} );
+self.addEventListener( 'fetch', function ( event ) {
+    var fetchRequest = event.request.clone();
+    const url = new URL( fetchRequest.url );
+    if ( url.pathname == "/fa.css" ) {
+        event.respondWith(
+            caches.match( event.request )
+            .then( function ( response ) {
+                //cache hit
+                if ( response )
+                    return response;
+                //BEGIN not in cache
+
+                return (
+                        /*Do a fresh network request for the url requested*/
+                        fetch( fetchRequest )
+                    )
+                    .then( function ( response ) {
+                        //check if network request generated a 200 OK and / or is a same Origin Request - DO NOT CACHE if not
+                        if ( !response || response.status !== 200 || response.type !== 'basic' )
+                            return response;
+                        var responseToCache = response.clone();
+                        caches.open( Externals )
+                            .then( function ( cache ) {
+                                cache.put( fetchRequest, responseToCache );
+                            } );
+                        return response;
+                    } );
+                //END not in cache
+            } )
+        );
+    }
 } );
